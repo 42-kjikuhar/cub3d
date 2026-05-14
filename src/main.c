0bde88ac39cb4080b0ca0355b0064cdc3f28db28
jiku0730 <kei2003730@gmail.com>
2026-05-14 15:49:51 +0900

    feat: main.cに全体の構造を追加。

--- src/main.c
+++ src/main.c
@@ -6,16 +6,29 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/14 13:25:52 by kjikuhar          #+#    #+#             */
-/*   Updated: 2026/05/14 15:14:39 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/05/14 15:49:01 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+#define SUCCESS 0
+#define FAILURE 1
+
+typedef struct s_cub3d
+{
+
+}				t_cub3d;
 
 int main(int argc, char const *argv[])
 {
-	(void)argc;
-	(void)argv;
+	t_cub3d	sim;
+
 	printf("Hello World!\n");
+	if (parse_args(argc, argv, &sim) != SUCCESS)
+		return (EXIT_FAILURE);
+	if (parse_maps(&sim))
+		return (EXIT_FAILURE);
+	running_sim(&sim);
+	free_simulation(&sim);
 	return (0);
 }
