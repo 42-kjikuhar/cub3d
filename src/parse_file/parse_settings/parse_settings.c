98bb7a1f09c5f0a44cf877dca0d9690e3da4b32e
jiku0730 <github@jiku.jp>
2026-05-16 16:14:18 +0900

    refactor: set_textureとset_colorのファイルを分割。

--- src/parse_file/parse_settings/parse_settings.c
+++ src/parse_file/parse_settings/parse_settings.c
@@ -6,7 +6,7 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 13:29:17 by kjikuhar          #+#    #+#             */
-/*   Updated: 2026/05/16 14:42:36 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/05/16 16:12:49 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,105 +48,6 @@ static char *read_next_setting_line(int fd)
 static void	cleanup_settings(t_settings *settings)
 {
 
-}
-
-static bool	set_texture(char const *value, t_settings *settings,
-	enum e_setting_id texture_id)
-{
-	char	*texture_path;
-
-	if (settings->flags & (1 << texture_id))
-	{
-		print_error("double setting is exist");
-		return (false);
-	}
-	settings->flags |= (1 << texture_id);
-	texture_path = ft_strdup(value);
-	if (texture_path == NULL)
-	{
-		print_error(strerror(errno));
-		return (false);
-	}
-	if (texture_id == NORTH_ID)
-		settings->north_texture = texture_path;
-	else if (texture_id == SOUTH_ID)
-		settings->south_texture = texture_path;
-	else if (texture_id == WEST_ID)
-		settings->west_texture = texture_path;
-	else if (texture_id == EAST_ID)
-		settings->east_texture = texture_path;
-	return (true);
-}
-
-enum e_color_channel
-{
-	RED,
-	GREEN,
-	BLUE
-};
-
-static bool	parse_color_channel(char const **value, int *color_channel)
-{
-	if (!ft_isdigit((*value)[0]))
-	{
-		print_error("color format is invalid");
-		return (false);
-	}
-	if ((*value)[0] == '0' && ft_isdigit((*value)[1]))
-	{
-		print_error("reading 0 is invalid");
-		return (false);
-	}
-	*color_channel = 0;
-	while (ft_isdigit(**value))
-	{
-		*color_channel = *color_channel * 10 + (**value - '0');
-		if (*color_channel > 255)
-		{
-			print_error("color value range needs 0~255");
-			return (false);
-		}
-		++(*value);
-	}
-	return (true);
-}
-
-static bool	parse_color(char const *value, int *color)
-{
-	int						color_channel;
-	enum e_color_channel	i;
-
-	i = RED;
-	while (i <= BLUE)
-	{
-		if (!parse_color_channel(&value, &color_channel))
-			return (false);
-		*color |= (color_channel << i * 8);
-		if ((i != BLUE && *value != ',') || (i == BLUE && *value != '\0'));
-			return (false);
-		++value;
-	}
-	return (true);
-}
-
-static bool	set_color(char const *value, t_settings *settings,
-	enum e_setting_id color_id)
-{
-	int	color;
-
-	if (settings->flags & (1 << color_id))
-	{
-		print_error("double setting is exist");
-		return (false);
-	}
-	settings->flags |= (1 < color_id);
-	if (!parse_color(value, &color))
-		return (false);
-	if (color_id == FLOOR_ID)
-		settings->floor_color = color;
-	else if (color_id == CEILING_ID)
-		settings->south_texture = color;
-	return (true);
 }
 
 static bool	assign_setting(char const *identifier, char const *value,
