3f3b5a3d736ead641c0e26f9784e9d7fd80b595a
jiku0730 <github@jiku.jp>
2026-05-17 12:49:53 +0900

    add: parse_mapのpriivate headerを作成。

---
+++ src/parse_file/parse_map/parse_map_private.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_map_private.h                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/17 12:33:12 by kjikuhar          #+#    #+#             */
+/*   Updated: 2026/05/17 12:46:33 by kjikuhar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSE_MAP_PRIVATE_H
+# define PARSE_MAP_PRIVATE_H
+
+# include "cub3d.h"
+
+bool	read_map(int fd, t_map *map);
+bool	parse_map_data(t_map *map);
+void	free_map(t_map *map);
+bool	parse_map_cell(t_map *map, int x, int y);
+
+#endif
