e740b8cdc8f5b820a294e9facf8531b9bb962737
jiku0730 <github@jiku.jp>
2026-05-17 12:01:11 +0900

    refactor: validate_argumentをファイル分割。

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/14 13:25:52 by kjikuhar          #+#    #+#             */
-/*   Updated: 2026/05/16 23:03:09 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/05/17 12:00:09 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,6 @@ void	print_error(char const *error_message)
 {
 	ft_dprintf(STDERR_FILENO, "Error\n");
 	ft_dprintf(STDERR_FILENO, "%s\n", error_message);
-}
-
-bool	is_valid_argument(int argc, char const *argv[])
-{
-	char	*filename;
-	size_t	len;
-
-	if (argc != 2)
-		return (false);
-	filename = ft_strrchr(argv[1], '/');
-	if (filename != NULL)
-		++filename;
-	else
-		filename = (char *)argv[1];
-	len = ft_strlen(filename);
-	if (len <= 4 || ft_strcmp(filename + len - 4, ".cub") != 0)
-		return (false);
-	return (true);
-}
-
-bool	vaildate_argument(int argc, char const *argv[])
-{
-	if (!is_valid_argument(argc, argv))
-	{
-		ft_dprintf(STDERR_FILENO, "Error\n");
-		ft_dprintf(STDERR_FILENO, "usage: %s *.cub\n", argv[0]);
-		return (false);
-	}
-	return (true);
 }
 
 int	main(int argc, char const *argv[])
