b01d00087f22aca7e6ef95c9d8299d19eaa0144e
jiku0730 <github@jiku.jp>
2026-05-19 22:03:11 +0900

    fix: ここでErrorが直接発生することはないので、ErrorMessageは必要なし。

--- src/parse_file/parse_map/parse_map.c
+++ src/parse_file/parse_map/parse_map.c
@@ -6,7 +6,7 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 18:36:09 by kjikuhar          #+#    #+#             */
-/*   Updated: 2026/05/19 21:52:12 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/05/19 22:02:41 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,7 @@
 bool	parse_map(int fd, t_map *map)
 {
 	if (!read_map(fd, map))
-	{
-		print_error("read_map_error");
 		return (false);
-	}
 	if (!parse_map_data(map))
 	{
 		cleanup_map(map);
