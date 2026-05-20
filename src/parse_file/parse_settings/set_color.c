d86454d247dc9d811c5285f9720eb828305d6d88
takau-jp <takausoccer@icloud.com>
2026-05-21 00:10:41 +0900

    fix: set_color

--- src/parse_file/parse_settings/set_color.c
+++ src/parse_file/parse_settings/set_color.c
@@ -6,7 +6,7 @@
 /*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/16 16:11:18 by kjikuhar          #+#    #+#             */
-/*   Updated: 2026/05/21 00:08:48 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/05/21 00:10:17 by stanaka2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ bool	set_color(char const *value, t_settings *settings,
 		print_error("double setting is exist");
 		return (false);
 	}
-	settings->flags |= (1 < color_id);
+	settings->flags |= (1 << color_id);
 	if (!parse_color(value, &color))
 		return (false);
 	if (color_id == FLOOR_ID)
