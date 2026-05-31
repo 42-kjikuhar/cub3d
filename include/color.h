e8dea769218c56b8eae3e3219847433f0ee8788b
takau-jp <takausoccer@icloud.com>
2026-06-01 00:02:35 +0900

    add: colorモジュール

--- include/color.h
+++ include/color.h
@@ -6,7 +6,7 @@
 /*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/29 17:48:04 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/05/29 18:11:56 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/05/31 22:27:31 by stanaka2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@ enum e_color_id
 	COLOR_FLOOR,
 	COLOR_CEILING,
 	COLOR_COUNT,
-}
+};
+
+int		get_color(enum e_color_id id);
+void	set_color(enum e_color_id id, int color);
 
 #endif
--- src/color/color.c
+++ src/color/color.c
@@ -6,7 +6,7 @@
 /*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/29 17:38:56 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/05/30 12:49:32 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/05/31 22:04:25 by stanaka2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 static int	g_colors[COLOR_COUNT];
 
-int	color(enum e_color_id id)
+int	get_color(enum e_color_id id)
 {
 	return (g_colors[id]);
 }
 
-void	set_color(enum e_color_id id, int rgb)
+void	set_color(enum e_color_id id, int color)
 {
-	g_colors[id] = rgb;
+	g_colors[id] = color;
 }
-
