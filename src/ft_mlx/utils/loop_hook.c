8b462ce72269c772f7f5645e80c8de9d5c51dee7
takau-jp <takausoccer@icloud.com>
2026-06-02 01:10:00 +0900

    fix: DEBUG

--- src/ft_mlx/utils/loop_hook.c
+++ src/ft_mlx/utils/loop_hook.c
@@ -6,18 +6,31 @@
 /*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 16:49:56 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/05/30 13:39:08 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/06/02 01:03:14 by stanaka2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+#ifndef DEBUG
 
 int	loop_hook(void *param)
 {
 	(void)param;
 	do_player_actions();
 	drawer();
-	if (DEBUG)
-		mlx_loop_end(get_mlx_ptr());
 	return (0);
 }
+
+#else
+
+int	loop_hook(void *param)
+{
+	(void)param;
+	do_player_actions();
+	drawer();
+	mlx_loop_end(get_mlx_ptr());
+	return (0);
+}
+
+#endif
