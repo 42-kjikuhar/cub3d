164be9b811a0f1869ade588d078330f466275654
jiku0730 <github@jiku.jp>
2026-06-21 19:20:24 +0900

    add: 左右キーで回転する処理の雛形を追加した。

--- src/ft_mlx/utils/key_press_hook.c
+++ src/ft_mlx/utils/key_press_hook.c
@@ -3,15 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   key_press_hook.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 16:50:32 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/05/22 17:53:03 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/06/21 19:20:24 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+static void	player_rotate(t_player *player)
+{
+	player->dir = dvec3_rotate(player->dir, dvec)
+}
 int	key_press_hook(int keycode, void *param)
 {
 	t_cub3d	*cub3d;
@@ -19,5 +23,8 @@ int	key_press_hook(int keycode, void *param)
 	cub3d = (t_cub3d *)param;
 	if (keycode == XK_Escape)
 		mlx_loop_end(cub3d->mlx.mlx_ptr);
+	if (keycode == XK_Left)
+	if (keycode == XK_Right)
+
 	return (0);
 }
