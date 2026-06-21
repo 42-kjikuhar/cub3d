969e15da2160c1e7f68dc318740d4ef8ae315116
jiku0730 <github@jiku.jp>
2026-06-21 19:24:29 +0900

    add: 左右キーで視点を回転する処理を実装した。

--- src/ft_mlx/utils/key_press_hook.c
+++ src/ft_mlx/utils/key_press_hook.c
@@ -6,16 +6,24 @@
 /*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 16:50:32 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/06/21 19:20:24 by kjikuhar         ###   ########.fr       */
+/*   Updated: 2026/06/21 19:23:01 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void	player_rotate(t_player *player)
+static void	player_rotate_left(t_player *player)
 {
-	player->dir = dvec3_rotate(player->dir, dvec)
+	player->dir = dvec3_rotate(player->dir, dvec3(0, 0, 1), 1);
+	player->plane = dvec3_rotate(player->plane, dvec3(0, 0, 1), 1);
 }
+
+static void	player_rotate_right(t_player *player)
+{
+	player->dir = dvec3_rotate(player->dir, dvec3(0, 0, 1), -1);
+	player->plane = dvec3_rotate(player->plane, dvec3(0, 0, 1), -1);
+}
+
 int	key_press_hook(int keycode, void *param)
 {
 	t_cub3d	*cub3d;
@@ -24,7 +32,8 @@ int	key_press_hook(int keycode, void *param)
 	if (keycode == XK_Escape)
 		mlx_loop_end(cub3d->mlx.mlx_ptr);
 	if (keycode == XK_Left)
+		player_rotate_left(&(cub3d->player));
 	if (keycode == XK_Right)
-
+		player_rotate_right(&(cub3d->player));
 	return (0);
 }
