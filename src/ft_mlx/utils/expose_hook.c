217287dcf39cb5c7a00f8c2b65e4728019d2e590
jiku0730 <github@jiku.jp>
2026-06-21 18:20:04 +0900

    rm: expose_hook のデバッグ描画を削除した。

--- src/ft_mlx/utils/expose_hook.c
+++ src/ft_mlx/utils/expose_hook.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expose_hook.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stanaka2 <stanaka2@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: kjikuhar <kjikuhar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/22 17:32:12 by stanaka2          #+#    #+#             */
-/*   Updated: 2026/05/22 17:46:30 by stanaka2         ###   ########.fr       */
+/*   Updated: 2026/06/21 18:19:18 by kjikuhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,6 @@ int	expose_hook(void *param)
 	t_cub3d	*cub3d;
 
 	cub3d = (t_cub3d *)param;
-	// (void)cub3d;
-	mlx_put_image_to_window(cub3d->mlx.mlx_ptr, cub3d->mlx.win_ptr, cub3d->mlx.assets.north_wall.img_ptr, 0, 0);
-	mlx_put_image_to_window(cub3d->mlx.mlx_ptr, cub3d->mlx.win_ptr, cub3d->mlx.assets.south_wall.img_ptr, TEXTURE_SIZE, 0);
-	mlx_put_image_to_window(cub3d->mlx.mlx_ptr, cub3d->mlx.win_ptr, cub3d->mlx.assets.west_wall.img_ptr, 0, TEXTURE_SIZE);
-	mlx_put_image_to_window(cub3d->mlx.mlx_ptr, cub3d->mlx.win_ptr, cub3d->mlx.assets.east_wall.img_ptr, TEXTURE_SIZE, TEXTURE_SIZE);
+	(void)cub3d;
 	return (0);
 }
